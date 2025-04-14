a71a9c484b4f93977768805d014165907b254e8b
JJKing <18182172334@163.com>
2025-04-14 22:13:13 +0800

    update

--- tools/sqlite_tool.py
+++ tools/sqlite_tool.py
@@ -4,35 +4,56 @@ import sqlite3
 from tools.file_tool import *
 
 
-def create_db():
-    try:
-        database_name = get_main_db_path()
+class SqlLiteDb(object):
 
-        # 先检查文件是否存在
-        db_existed = os.path.exists(database_name)
+    def __init__(self):
+        self._connection = None
 
-        connection = sqlite3.connect(database_name)
-        cursor = connection.cursor()
+    def __enter__(self):
+        self.create_db()
+        return self
 
-        # 检查是否有表存在
-        cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
-        tables = cursor.fetchall()
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        # 释放资源
+        self._connection.close()
+        print("数据库关闭")
+        if exc_type is not None:
+            # 处理异常
+            print(f"Exception: {exc_type}, {exc_val}")
+        return False
 
-        if db_existed:
-            if tables:
-                print(f"数据库已存在且包含 {len(tables)} 个表: {database_name}")
+    def create_db(self):
+        try:
+            database_name = get_main_db_path()
+            # 先检查文件是否存在
+            db_existed = os.path.exists(database_name)
+            self._connection = sqlite3.connect(database_name)
+            cursor = self._connection.cursor()
+            # 检查是否有表存在
+            cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+            tables = cursor.fetchall()
+
+            if db_existed:
+                if tables:
+                    print(f"数据库已存在且包含 {len(tables)} 个表: {database_name}")
+                else:
+                    print(f"数据库已存在但不包含任何表: {database_name}")
             else:
-                print(f"数据库已存在但不包含任何表: {database_name}")
-        else:
-            print(f"新建数据库文件: {database_name}")
-            if tables:  # 理论上新建的数据库不应该有表
-                print(f"警告: 新建的数据库包含 {len(tables)} 个表")
+                print(f"新建数据库文件: {database_name}")
+                if tables:  # 理论上新建的数据库不应该有表
+                    print(f"警告: 新建的数据库包含 {len(tables)} 个表")
+            # connection.close()
 
-        connection.close()
+        except sqlite3.Error as error:
+            print("数据库错误: ", error)
 
-    except sqlite3.Error as error:
-        print("数据库错误: ", error)
+    def execute_sql(self, str=None):
+        reuslt = self._connection.cursor().execute("SELECT name FROM sqlite_master WHERE type='table';")
+        print(reuslt)
 
 
 if __name__ == '__main__':
-    create_db()
+    with SqlLiteDb() as db:
+        db.execute_sql()
+
+
